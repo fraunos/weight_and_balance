f0438d1109fe0804cfcd4fcfa4fbeab0c08aed3f
Michał Wołoszyn <fraunos@gmail.com>
2022-04-12 09:12:15 +0200

    use Vue

--- public/App.js
+++ public/App.js
@@ -1,12 +1,37 @@
 export default {
   data() {
     return { 
-      count: 0,
       selectedPlane: "",
-      czop: [1,2,3,4,5,6]
+      planesList: [],
+      planeData: {}
     }
   },
+  async mounted(){
+    this.planesList = await requestJSON('/planes')
+  },
   methods: {
-    
+    async getPlaneData(ev) {
+      console.log(ev)
+      this.planeData = await requestJSON(ev.target.value)
+    },
+    loadWeight({density = 1, value}) {
+      return Math.round(density * value * 10) / 10
+    }
+  },
+  computed: {
+    totalWeight() {
+      const sum =  this.planeData.loads?.map(i=>({density: i.density ?? 1, value: i.value}))
+        .filter(i=>i.density && i.value)
+        .reduce((acc,cv)=>{
+          return cv.density * cv.value + acc
+      }, 0)
+      return sum
+    }
   }
+}
+
+async function requestJSON(url){
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
 }
